761464cd61b0f16abd20e0435c2a9872eefd6514
Nahid Hasan <nahidn228@gmail.com>
2025-03-17 22:51:57 +0600

    tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
